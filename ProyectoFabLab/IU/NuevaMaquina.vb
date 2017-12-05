2f30994885cacb4c7cee7aab5cab713b175ff4ad
alejandro <alejandro.pons98@gmail.com>
2017-12-05 14:54:13 +0100

    Nomenclatura nombres de variables. (Para Pablo)

--- ProyectoFabLab/IU/NuevaMaquina.vb
+++ ProyectoFabLab/IU/NuevaMaquina.vb
@@ -1,4 +1,6 @@
-﻿Public Class NuevaMaquina
+﻿Imports System.Data.SqlClient
+
+Public Class NuevaMaquina
     Private Sub CancelarButton_Click(sender As Object, e As EventArgs) Handles CancelarButton.Click
 
         Me.Close()
@@ -7,7 +9,20 @@
 
     Private Sub NuevaMaquina_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-
+        CargarTiposMaquinas()
 
     End Sub
+
+    Private Sub CargarTiposMaquinas()
+
+        Dim tablaTiposMaquinas As SqlDataReader = NegocioTiposMaquinas.ObtenerTiposMaquinas()
+
+        While tablaTiposMaquinas.Read()
+
+            TipoMaquinaComboBox.Items.Add(tablaTiposMaquinas.GetString(0))
+
+        End While
+
+    End Sub
+
 End Class
