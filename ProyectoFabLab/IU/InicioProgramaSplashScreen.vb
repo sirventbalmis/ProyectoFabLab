9c60acbe43e383463081f9e004ace042cc95f26b
Alejandro <alejandro.pons98@gmail.com>
2017-12-11 09:57:08 +0100

    Nuevo Cambio

--- ProyectoFabLab/IU/InicioProgramaSplashScreen.vb
+++ ProyectoFabLab/IU/InicioProgramaSplashScreen.vb
@@ -5,24 +5,9 @@
 
 
     Private Sub SplashScreen1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        'Configure el texto del cuadro de diálogo en tiempo de ejecución según la información del ensamblado de la aplicación.  
-
-        'TODO: Personalice la información del ensamblado de la aplicación en el panel "Aplicación" del cuadro de diálogo 
-        '  propiedades del proyecto (bajo el menú "Proyecto").           
 
 
-        'Dé formato a la información de versión usando el texto establecido en el control de versiones en tiempo de diseño como
-        '  cadena de formato.  Esto le permite una localización efectiva si lo desea.
-        '  Se pudo incluir la información de compilación y revisión usando el siguiente código y cambiando el 
-        '  texto en tiempo de diseño del control de versiones a "Versión {0}.{1:00}.{2}.{3}" o algo parecido.  Consulte
-        '  String.Format() en la Ayuda para obtener más información.
-        '
-        '    Version.Text = System.String.Format(Version.Text, My.Application.Info.Version.Major, My.Application.Info.Version.Minor, My.Application.Info.Version.Build, My.Application.Info.Version.Revision)
 
-        ' Version.Text = ""
-
-        'Información de Copyright
-        ' Copyright.Text = My.Application.Info.Copyright
     End Sub
-    'ponsito
+
 End Class
